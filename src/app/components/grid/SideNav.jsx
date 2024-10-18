60f2802e20b5ef59fc9601285bebfe02d00cca95
Kirill Ginko <53977030+kirillginko@users.noreply.github.com>
2024-10-18 15:45:12 -0400

    added sidenav

---
+++ src/app/components/grid/SideNav.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "../../styles/gridlayout.module.css";
+
+const SideNav = () => {
+  return (
+    <div className={styles.sideNav}>
+      {/* Add your content here */}
+      <p>Side Navigation Content</p>
+    </div>
+  );
+};
+
+export default SideNav;
